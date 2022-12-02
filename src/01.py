869b3bb939515dacc394e32c7476b2ff10933613
Leiv-Erik Rosseland <leiv-erik.rosseland@spk.no>
2022-12-02 16:18:36 +0100

    Setup and first day solution

---
+++ src/01.py
@@ -0,0 +1,21 @@
+from utils.api import get_input
+from functools import reduce
+
+input_str = get_input(1)
+
+elf_calory_counts = [
+    reduce(
+        lambda acc, food: acc+food, 
+        map(
+            lambda food: int(food.strip()), 
+            food_list.strip().split("\n")
+        )
+    ) 
+    for food_list 
+    in input_str.strip().split("\n\n")
+]
+
+elf_calory_counts.sort(reverse=True)
+
+print(f"Solution part 1: {str(elf_calory_counts[0])}")
+print(f"Solution part 2: {str(elf_calory_counts[0] + elf_calory_counts[1] + elf_calory_counts[2])}")
